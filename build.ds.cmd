917ac19c3856b6c01748c0cfd1d5c7d3e80b2c22
js <js.seth.h@gmail.com>
2015-02-02 01:50:07 +0900

    ds용 빌드 도구 추가.

---
+++ build.ds.cmd
@@ -0,0 +1,1 @@
+powershell.exe -noprofile -executionpolicy bypass -File make.ds.ps1
---
+++ make.ds.ps1
@@ -0,0 +1,13 @@
+
+"to-be-a-fighter","to-do-chores" | ForEach-Object {
+	 
+	Remove-Item "ds-$($_)"  -Force -Recurse 
+	copy-item $_ "ds-$($_)" -force -recurse -verbose
+
+	cd "ds-$($_)" 
+	(Get-Content modinfo.lua) | Foreach-Object {$_ -replace 'api_version = 10','api_version = 6'}  | Out-File modinfo.lua
+	cd ..
+
+}
+
+ 
